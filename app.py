97a4e9a4b15b8ca167fcce41cee6d2fe46aac760
Vidip <>
2020-03-07 20:34:59 -0400

    source_city

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import connection
 import boto
 
@@ -19,24 +19,43 @@ def database_connection():
 	print(abc.fetchall())
 	return render_template('index.html')
 
-@app.route("/source/city")
-def database_connection():
+@app.route("/source/city",methods=['GET'])
+def check_source_city():
+	if 'id' in request.args:
+		id=request.args['id']
+		abc = connection.connection_manager()
+		sql = """ 
+		SELECT * FROM cities_lookup WHERE cityid = ?
+		"""
+		abc.execute("SELECT cityid,cityname FROM cities_lookup WHERE cityid = %s",[id])
+		results = abc.fetchall()
+		for res in results:
+			cityname = res[1]
+			cityid = res[0]
+	else:
+		print("Error")
+
 	return render_template('index.html')
 
 @app.route("/destination")
-def database_connection():
+def destination_search():
 	return render_template('index.html')
 
 @app.route("/book/ticket")
-def database_connection():
+def book_ticket():
 	return render_template('index.html')
 
 @app.route("/get/report")
-def database_connection():
+def get_report():
 	return render_template('index.html')
 
 @app.route("/bus/schedule")
-def database_connection():
+def get_schedule():
+	return render_template('index.html')
+
+@app.route("/sendfiletos3")
+def put_file_to_s3():
+	conn = S3Connection('<aws access key>', '<aws secret key>')
 	return render_template('index.html')
 
 if __name__ == "__main__":
